d55dcd407740c1bbe52f05d834ad76c3c5a83a6d
Chinnatip Taemkaeo <chinnatip@me.com>
2024-02-09 18:49:16 +0700

    intro to react

---
+++ src/app/intro/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+const Page = () => {
+    const itemLists = [
+        {id: 1, title: 'apple'},
+        {id: 2, title: 'grape'},
+        {id: 3, title: 'banana'},
+    ]    
+    const logger = (fruitName: string) => {
+        alert(`You just click ${fruitName}`)
+    }
+    return <div className="p-12">
+        {/* Title */}
+        <h1 className="text-2xl">Hello world</h1>
+        <hr className="my-4" />
+        <ul className="text-yellow-400 list-disc">
+            { itemLists.map(item => <div>{item.title}</div> ) }
+        </ul>
+        <hr className="my-4" />
+        {/* Table */}
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>item</th>
+                    <th>action</th>
+                </tr>
+            </thead>
+            <tbody>
+                { itemLists.map(item => {
+                    const {id , title} = item
+                    return <tr>
+                        <td className="border border-white p-2">{id}</td>
+                        <td className="border border-white p-2">{title}</td>
+                        <td className="border border-white p-2">
+                            <button 
+                                onClick={() => logger(title)}
+                                className="bg-blue-500 text-white rounded-lg px-4 py-2">save</button>
+                        </td>
+                    </tr>
+                }) }
+            </tbody>
+        </table>
+        <hr className="my-4" />
+        {/* Debug */}
+        <div>{JSON.stringify(itemLists)}</div>
+    </div>
+}
+
+export default Page
